05bb0f27cbe79561464fad59145be33f8e0aa322
lopezabraham <abrahamlopezsierra@iestorredelrey.es>
2024-04-25 09:14:58 +0200

    hemos añadido los datos para la tabla pasajero

--- insert aeropuerto.sql
+++ insert aeropuerto.sql
@@ -6,4 +6,27 @@ insert into Marca (Nombre) values
 ("atr"),
 ("mitsubishi"),
 ("comac"),
-("irkut")
+("irkut");
+
+insert into Modelos (Nombre, Marca) values
+('737', 1),
+('A320', 2),
+('E190', 3),
+('CRJ1000', 4),
+('ATR-72', 5),
+('SpaceJet M90', 6),
+('C919', 7),
+('MC-21', 8);
+
+insert into Aeropuertos (Nombre, Ciudad) values
+('Aeropuerto Internacional JFK', 'Nueva York'),
+('Aeropuerto de Heathrow', 'Londres'),
+('Aeropuerto Internacional de Frankfurt', 'Frankfurt'),
+('Aeropuerto de Charles de Gaulle', 'París');
+
+insert into Aviones (Nombre, IdModelo, IdAerolinea) values
+('raianer', 1, 1),
+('iberia', 2, 2),
+('vueling', 3, 3),
+('qatar', 4, 4);
+
