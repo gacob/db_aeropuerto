546300a098b8ae72826f042fa8e75587b7e55a3a
PepeCarmona <jcargav@g.educaand.es>
2024-05-03 09:02:07 +0200

    agregado un trigger que actualiza asientos

--- triggers.sql
+++ triggers.sql
@@ -12,3 +12,32 @@ BEGIN
     );
 END//
 DELIMITER ;
+
+-- actualiza asientos
+DELIMITER //
+CREATE TRIGGER actualizar_asientos_disponibles
+AFTER INSERT ON Ticket
+FOR EACH ROW
+BEGIN
+    UPDATE Vuelo
+    SET AsientosDisponibles = AsientosTotales - (
+        SELECT COUNT(*) 
+        FROM Ticket 
+        WHERE IdVuelo = NEW.IdVuelo
+    )
+    WHERE Id = NEW.IdVuelo;
+END$$
+
+CREATE TRIGGER actualizar_asientos_eliminar
+AFTER DELETE ON Ticket
+FOR EACH ROW
+BEGIN
+    UPDATE Vuelo
+    SET AsientosDisponibles = AsientosTotales - (
+        SELECT COUNT(*) 
+        FROM Ticket 
+        WHERE IdVuelo = OLD.IdVuelo
+    )
+    WHERE Id = OLD.IdVuelo;
+END//
+DELIMITER ;
